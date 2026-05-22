4f4b3f9b9d554416e3ceb24f396e4ff6f8d5c840
David L. Inman <davidlevere@gmail.com>
2026-05-21 21:35:01 -0400

    inc prog

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 
 pub mod data;
+pub mod vm;
 
 
 #[cfg(test)]
--- src/vm.rs
+++ src/vm.rs
@@ -1,11 +1,18 @@
 
+use crate::data::CompiledProc;
+
 pub struct Vm {
     memory: Vec<u8>,
-    frames: Vec<u8>, // everything is a reference? vec<usize>
+    frames: Vec<usize>, 
+    code: Vec<CompiledProc>,
 }
 
 impl Vm {
-    fn run(&mut self, entry : usize) -> Result<> {
+    pub fn new(code : Vec<CompiledProc>) -> Vm {
+        Vm { code, frames: vec![], memory: vec![] }
+    }
 
+    pub fn run(&mut self, entry : usize) -> Result<usize, String> {
+        Ok(0)
     }
 }
